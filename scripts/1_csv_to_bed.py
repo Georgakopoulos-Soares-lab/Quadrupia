80cd8b7babbe2569ffad552ec2bc1c7b81484f2c
Akshatha Nayak <akshanayaki@gmail.com>
2024-01-28 16:28:24 -0500

    convert csv to bed

---
+++ scripts/1_csv_to_bed.py
@@ -0,0 +1,49 @@
+import os
+import argparse
+import pandas as pd
+from fuc import pybed
+
+# Base path
+BASE_PATH = "/storage/group/izg5139/default/akshatha/gquad"
+# Directory for temporary bed files
+TEMP_PATH = f"{BASE_PATH}/temp"
+
+def csv_to_bed(csv_path, file, result_path):
+    csv_file = os.path.join(csv_path, file)
+    temp_bed_file = os.path.join(TEMP_PATH, file.replace('.csv', '.temp.bed'))
+    merged_file = os.path.join(result_path, file.replace('.csv', '.bed'))
+    try:
+        df = pd.read_csv(csv_file)
+        df = df.drop('filename', axis=1)
+        df = df.rename(columns={'chromosome': 'Chromosome', 'start': 'Start', 'end': 'End', 'sequence': 'Sequence', 'length': 'Length'})
+        # delete score and nbr column
+        df = df.drop('score', axis=1)
+        df = df.drop('nbr', axis=1)
+        
+        print(f"Converting {csv_file}")
+        bf = pybed.BedFrame.from_frame(meta=[], data=df)
+        bf.to_file(temp_bed_file)
+
+        # sort and merge bed file
+        cmd = f"sort -k1,1 -k2,2n {temp_bed_file} | bedtools merge > {merged_file}"
+        os.system(cmd)
+        
+        # remove temporary bed file
+        os.remove(temp_bed_file)
+        
+    except Exception as e:
+        print(f"Error reading {csv_file}: {e}")
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Convert csv to bed files")
+    parser.add_argument("csv_path", type=str, help="Path to directory containing CSV files")
+    parser.add_argument("file_name", type=str, help="Assembly file name")
+    parser.add_argument("bed_path", type=str, help="Path to save bed files")
+    cli_args = parser.parse_args()
+    
+    # directory for resulting merged bed files
+    cmd = f"mkdir -p {cli_args.bed_path}"
+    os.system(cmd)
+    
+    csv_to_bed(cli_args.csv_path, cli_args.file_name, cli_args.bed_path)
+    
---
+++ slurm/prep/1_args_csv_to_bed.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+BASE_PATH = "/storage/group/izg5139/default/akshatha/gquad"
+
+g4hunter_path = f"{BASE_PATH}/raw_data/g4hunter"
+g4hunter_nonempty_list = f"{BASE_PATH}/slurm/files/g4hunter_nonempty_list.txt"
+g4hunter_result_path = f"{BASE_PATH}/data/g4hunter_bed"
+df_g4hunter = pd.read_csv(g4hunter_nonempty_list, sep="\t", header=None)
+df_g4hunter.columns = ["filename"]
+df_g4hunter["path"] = g4hunter_path
+df_g4hunter["result_path"] = g4hunter_result_path
+
+regex_path = f"/storage/group/izg5139/default/external/genomes/all_genomes/regex/regex_gresults"
+regex_nonempty_list = f"{BASE_PATH}/slurm/files/regex_nonempty_list.txt"
+regex_result_path = f"{BASE_PATH}/data/regex_bed"
+df_regex = pd.read_csv(regex_nonempty_list, sep="\t", header=None)
+df_regex.columns = ["filename"]
+df_regex["path"] = regex_path
+df_regex["result_path"] = regex_result_path
+
+df = pd.concat([df_g4hunter, df_regex])
+# reorder columns to match the order in which they are passed to the script
+df = df[["path", "filename", "result_path"]]
+# save as txt file with space as delimiter
+df.to_csv(f"{BASE_PATH}/slurm/files/csv_to_bed_args.txt", sep=" ", index=False, header=False)
